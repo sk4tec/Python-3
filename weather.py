7c08a8d0a77cd44b7e54218d2bbe025e8db73227
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2026-01-29 16:41:52 +0000

    Update weather.py

--- weather.py
+++ weather.py
@@ -2,9 +2,10 @@ temperature = int(input("Enter the temperature in Celsius: "))
 
 if temperature > 26:
     print("It's too hot!")
-    print("Stay inside!")
 elif temperature < 15:
     print("It's too cold!")
+
+if temperature > 26 or temperature < 15:
     print("Stay inside!")
 else:
     print("The weather is just right!")
