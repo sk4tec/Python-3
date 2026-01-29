8f0a8486724cfc788204462a59943a7906ae1715
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2026-01-29 16:17:22 +0000

    Update age.py

--- age.py
+++ age.py
@@ -1,6 +1,8 @@
 age = input("Enter your age: ")
 age = int(age)
 
-decades = (int(age / 10))
+decades = (int(age / 10)) # convert to integer to remove decimal places
+decades = age // 10       # integer division to remove decimal places
+
 years = age % 10
 print("You have lived for", decades, "decades and", years, "years.")
