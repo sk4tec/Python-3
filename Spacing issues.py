b41c16d8667b138ecf61a8270c7e787434f2f239
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2026-01-29 16:48:21 +0000

    broken Python
    watch out for tabs vs spacing, trailing spaces etc...  ;o/

---
+++ Spacing issues.py
@@ -0,0 +1,17 @@
+x = 2
+
+if x == 1:
+    print("Option 1 selected")
+elif x == 2:
+    print("Option 2 selected")
+ elif x == 3:   # ← ONE extra space brakes it
+    print("Option 3 selected")
+
+
+if x:
+    print("tab")
+     print("spaces") # ← ONE extra space brakes it
+
+
+if x == 1:␣␣ # ← ONE extra trailing space brakes it
+    print("Option 1")
