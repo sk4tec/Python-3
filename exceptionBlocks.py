f995aaa7e84b148af63a8056f2d542e06aa50fc4
Sunjay Kalsi <sunjay.kalsi@gmail.com>
2026-02-03 09:25:36 +0000

    update

--- exceptionBlocks.py
+++ exceptionBlocks.py
@@ -3,7 +3,7 @@ acronyms = ["API", "HTTP", "CPU", "RAM"]
 try:
     definition = acronyms["CPU"]
 except:
-    print("Error: Only strings are allowed in the acronyms list.")
+    print("Exception: String was not foundin the acronyms list.")
 finally:
     print("The acronyms list contains:", acronyms)
 print("The program continues...")
